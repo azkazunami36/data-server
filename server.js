0f71a0b1c5c7c3ef6c78c7094b5535de6e4158cc
azkazunami36 <kazunami36.sum@icloud.com>
2022-10-22 14:21:28 +0900

    コード短縮

--- server.js
+++ server.js
@@ -1,13 +1,4 @@
-const express = require("express");
-const app = express();
-const fs = require("fs");
-const url = require("url");
-
-const filename = "data.json";
-
-if (!fs.existsSync(filename)) fs.writeFileSync(filename, JSON.stringify({}));
-
-const port = parseInt(process.env.PORT || "3000", 10);
+const app = require("express")(), fs = require("fs"), url = require("url"), filename = "data.json", port = parseInt(process.env.PORT || "3000", 10);
 
 app.listen(port, async () => {
     console.info("Listening on " + port);
@@ -15,7 +6,9 @@ app.listen(port, async () => {
 
 app.get("/*", async (req, res) => {
     console.log("GETリクエストを受信しました。");
+
     const param = url.parse(req.url);
+    if (param) console.log(param);
 
     res.header("Content-Type", "text/plain;charset=utf-8");
     res.end(fs.readFileSync(filename));
@@ -24,21 +17,19 @@ app.get("/*", async (req, res) => {
 app.post("/*", async (req, res) => {
     console.log("POSTリクエストを受信しました。");
 
-    var data = "";
-    req.on("data", function (chunk) {
-        data += chunk;
-    });
-    req.on("end", () => {
+    let data;
+    req.on("data", async chunk => data += chunk);
+    req.on("end", async () => {
+        if (!fs.existsSync(filename)) fs.writeFileSync(filename, JSON.stringify({}));
         const database = JSON.parse(fs.readFileSync(filename));
         const json = JSON.parse(data);
         if (json[0]) {
             console.log("要求: " + json[0]);
             database[json[0]] = json[1];
             fs.writeFileSync(filename, JSON.stringify(database, null, "  "));
-        } else {console.log("要求はありません。")};
+        } else console.log("要求はありません。");
 
         res.header("Content-Type", "text/plain;charset=utf-8");
         res.end(JSON.stringify(database));
     });
-
 });
